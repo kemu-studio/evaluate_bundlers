ae439bbf877962ffa83e567057475d1f6c112331
Sylwester Wysocki <dzik@ke.mu>
2022-05-18 11:48:34 +0200

    Added esbuild example.

---
+++ esbuild_test_01/src/libAnn.js
@@ -0,0 +1,34 @@
+const AnnInternalNumber = 10
+const AnnInternalFunction = () => {
+  for (let idx = 0; idx < AnnInternalNumber; idx++) {
+    console.log('The libAnn::AnnInternalFunction...')
+  }
+}
+
+export function emitAnnConsoleLog() {
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  console.log('The libAnn::emitAnnConsoleLog()...')
+  AnnInternalFunction()
+}
+
+export function emitAnnConsoleLog2() {
+  console.log('The libAnn::emitAnnConsoleLog2:1...')
+  console.log('The libAnn::emitAnnConsoleLog2:2...')
+  console.log('The libAnn::emitAnnConsoleLog2:3...')
+  console.log('The libAnn::emitAnnConsoleLog2:4...')
+  console.log('The libAnn::emitAnnConsoleLog2:5...')
+  console.log('The libAnn::emitAnnConsoleLog2:6...')
+  console.log('The libAnn::emitAnnConsoleLog2:7...')
+  console.log('The libAnn::emitAnnConsoleLog2:8...')
+  console.log('The libAnn::emitAnnConsoleLog2:9...')
+  console.log('The libAnn::emitAnnConsoleLog2:10...')
+  console.log('The libAnn::emitAnnConsoleLog2:11...')
+  console.log('The libAnn::emitAnnConsoleLog2:12...')
+  AnnInternalFunction()
+}
