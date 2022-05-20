9796ecbfe2572391f144c06cce8b0feadfc856a5
Sylwester Wysocki <dzik@ke.mu>
2022-05-20 11:57:21 +0200

    rollup: Added ifdef example.

--- rollup_test_01/rollup.config.js
+++ rollup_test_01/rollup.config.js
@@ -1,6 +1,10 @@
 import {terser} from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import consts from 'rollup-plugin-consts';
+import babel from 'rollup-plugin-babel';
+import replace from '@rollup/plugin-replace';
+import jscc from 'rollup-plugin-jscc'
 
 export default {
 //  input: 'src/index.js',
@@ -9,12 +13,32 @@ export default {
     exports: 'auto',
     file: 'bundle.js',
     format: 'iife',
+    //format: 'cjs',
     plugins: [terser()]
   },
   plugins: [
     commonjs(),
+    replace({
+      IS_BROWSER_FROM_REPLACE_PLUGIN: true
+    }),
+    jscc({
+      values: { _IS_BROWSER_FROM_JSCC_PLUGIN: true },
+    }),
     // https://github.com/rollup/plugins/tree/master/packages/node-resolve
     // Output imported modules to the bundle.
-    nodeResolve()
+    nodeResolve({
+      exportConditions: [
+        'default', 'module', 'import'
+      ]
+    }),
+    // https://www.npmjs.com/package/rollup-plugin-consts
+    // Define global constant.
+    // Usage: import x from 'consts:x'
+    consts({
+      IS_BROWSER_FROM_GLOBAL_CONFIG: true
+    }),
+    //babel({
+    //  exclude: 'node_modules/**'
+    //})
   ]
 };
--- rollup_test_01/src/indexRequireNodeOrBrowser.js
+++ rollup_test_01/src/indexRequireNodeOrBrowser.js
@@ -1,4 +1,5 @@
-/*
+import IS_BROWSER_FROM_GLOBAL_CONFIG from 'consts:IS_BROWSER_FROM_GLOBAL_CONFIG'
+
 function requireNodeOrBrowser(valueForNode, valueForBrowser) {
   if (IS_BROWSER_FROM_GLOBAL_CONFIG) {
     return valueForBrowser
@@ -13,6 +14,8 @@ console.log(K);
 
 // Global constant defined in webpack.config.js with DefinePlugin.
 // Dead branch should be eliminated by 'minimize' optimization in webpack.
+/*
+  REVIEW IT: Is it possible to resolve require() under rollupjs?
 
 if (IS_BROWSER_FROM_GLOBAL_CONFIG) {
   const browserOnly = require('./browserOnly.js')
@@ -24,18 +27,16 @@ if (IS_BROWSER_FROM_GLOBAL_CONFIG) {
   serverOnly.sayHelloServer(5678)
   console.log('Hello from server!')
 }
-
-// #IF / #ELSE / #ENDIF preprocessor handled by ifdef-loader defined in
-// webpack.config.js.
-
-/// #if IS_BROWSER_FOR_IFDEF
+*/
+// Conditional import.
+//#if _IS_BROWSER_FROM_JSCC_PLUGIN
   import { browserOnly } from './browserOnly.js'
   browserOnly.sayHelloBrowser(9000)
-/// #else
+//#else
   import { serverOnly } from './serverOnly.js'
   serverOnly.sayHelloServer(9001)
-/// #endif
-*/
+//#endif
+
 // Import polimorphic module.
 // Browser and server code should be distinguish basing on main/browser
 // entries in package.json of imported module.
