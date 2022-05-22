e946a32089ce3da04ae5681d04c6f1ae551e86e1
Sylwester Wysocki <dzik@ke.mu>
2022-05-22 18:07:18 +0200

    esbuild: added code splitting and watch example.

---
+++ test02/esbuild/packages/examplebsc/index-browser.js
@@ -0,0 +1,5 @@
+export default {
+  emitLog: function(x) {
+    console.log('[ exampleBSC/index-browser.js ]', x)
+  }
+}
---
+++ test02/esbuild/src/index2.js
@@ -0,0 +1,6 @@
+import exampleBSC from 'examplebsc'
+exampleBSC.emitLog('Hello from index2')
+
+import('./lib01.js')
+import('./lib02.js')
+import('./lib03.js')
